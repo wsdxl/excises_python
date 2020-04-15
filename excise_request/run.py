603b4eaecfddca80b7571701d754043cef9a3524
duxiaolei <abc4862556>
2020-04-15 13:17:22 +0800

    新增审核接口

--- excise_request/run.py
+++ excise_request/run.py
@@ -11,7 +11,7 @@ import unittest
 from library.HTMLTestRunnerNew import HTMLTestRunner
 from common.contants import REPORT_DIR,CASE_DIR
 from common.read_conf import conf
-from testcases import test_login,test_recharge,test_withdraw,test_register,test_add
+from testcases import test_login,test_recharge,test_withdraw,test_register,test_add,test_audit
 
 #第一步：创建测试套件
 suite=unittest.TestSuite()
@@ -19,7 +19,7 @@ suite=unittest.TestSuite()
 #第二步：加载测试用例到测试套件中
 loader=unittest.TestLoader()
 # suite.addTest(loader.discover(CASE_DIR))
-suite.addTest(loader.loadTestsFromModule(test_add))
+suite.addTest(loader.loadTestsFromModule(test_audit))
 # 第三步：创建启动程序
 report_path=os.path.join(REPORT_DIR,conf.get('report','filename'))
 with open(report_path,'wb') as f:
---
+++ excise_request/testcases/test_audit.py
@@ -0,0 +1,111 @@
+"""
+============================
+Author  : XiaoLei.Du
+Time    : 2020/4/15 10:42
+E-mail  : 506615839@qq.com
+file:   test_audit.py
+============================
+"""
+import unittest
+import os
+import jsonpath
+from library.ddt import ddt, data
+from common.readexcel import ReadExcel
+from common.contants import DATA_DIR
+from common.read_conf import conf
+from common.hande_request import HandleRequest
+from common.hande_data import CaseData, replace_data
+from common.mylogger import mylog
+from common.hande_db import Hande_DB
+
+file_path = os.path.join(DATA_DIR, 'cases.xlsx')
+
+
+@ddt
+class TestAudit(unittest.TestCase):
+    excel = ReadExcel(file_path, 'audit')
+    cases = excel.read_excel()
+    http = HandleRequest()
+    db=Hande_DB()
+
+    @classmethod
+    def setUpClass(cls):
+        url = conf.get('env', 'url') + '/member/login'
+        data = {"mobile_phone": conf.get('env', 'admin_phone'), "pwd": conf.get('env', 'admin_pwd')}
+        headers = eval(conf.get('env', 'headers'))
+        response = cls.http.send(url=url, method='post', json=data, headers=headers)
+        res = response.json()
+
+        # 提取token值和member_id
+        id = jsonpath.jsonpath(res, '$..id')[0]
+        setattr(CaseData, 'admin_member_id', str(id))
+        token_type = jsonpath.jsonpath(res, '$..token_type')[0]
+        token = jsonpath.jsonpath(res, '$..token')[0]
+        token_data = token_type + ' ' + token
+        setattr(CaseData, 'token_data', token_data)
+
+    def setUp(self):
+        url = conf.get('env', 'url') + '/loan/add'
+        data= {"member_id":CaseData.admin_member_id,
+                "title":"升职加薪",
+                "amount":5000,
+                "loan_rate":18,
+                "loan_term":12,
+                "loan_date_type":1,
+                "bidding_days":6}
+        headers=eval(conf.get('env','headers'))
+        headers['Authorization']=getattr(CaseData,'token_data')
+
+        response=self.http.send(url=url,method='post',json=data,headers=headers)
+        res=response.json()
+        # 提取id
+        id=jsonpath.jsonpath(res,'$..id')[0]
+        setattr(CaseData,'loan_id',str(id))
+
+
+
+    @data(*cases)
+    def test_audit(self, case):
+        # 准备测试数据
+        url=conf.get('env','url')+case['url']
+        case['data']=replace_data(case['data'])
+        data=eval(case['data'])
+        headers=eval(conf.get('env','headers'))
+        headers['Authorization'] = getattr(CaseData, 'token_data')
+        expected=eval(case['expected'])
+        method=case['method']
+        row=case['case_id']+1
+
+        # 发送请求
+        response = self.http.send(url=url, method=method, json=data, headers=headers)
+        res = response.json()
+        # 提取审核通过的member_id
+
+        if res['msg']=='OK' and res['status']==2:
+            id=jsonpath.jsonpath(res,'$..id')[0]
+            setattr(CaseData,'pass_loan_id',str(id))
+
+         # 断言
+        try:
+            self.assertEqual(expected['code'],res['code'])
+            self.assertEqual(expected['msg'], res['msg'])
+            if case['check_sql']:
+                sql=replace_data(case['check_sql'])
+                status=self.db.get_one(sql)[0]
+                self.assertEqual(expected['status'],status)
+
+        except AssertionError as e:
+            self.excel.write_excel(row=row, column=8, value='未通过')
+            mylog.info('用例：{}---->测试未通过'.format(case['title']))
+            print('预期结果：{}'.format(expected))
+            print('实际结果：{}'.format(res))
+            mylog.error(e)
+            raise e
+        else:
+            self.excel.write_excel(row=row, column=8, value='已通过')
+            mylog.info('用例：{}---->测试已通过'.format(case['title']))
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.db.close()
